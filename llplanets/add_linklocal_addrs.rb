595ac0b316331a7e246eb0d446226944b8074069
takano32 <mitsuhiro.takano@gree.co.jp>
2011-07-21 20:39:13 +0900

    generate IPv6 addresses to representate byte

--- llplanets/add_linklocal_addrs.rb
+++ llplanets/add_linklocal_addrs.rb
@@ -4,6 +4,7 @@ IF='en0'
 
 if __FILE__ == $0 then
 	1.upto 2**128 do |i|
+		break if i == 2**8
 		addr = sprintf("%X\n", i).reverse.scan(/.{1,4}/).reverse.map do |hex|
 			hex.reverse
 		end.join(':')
