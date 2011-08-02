f3db99fa1cafe35159b27f2226533409e9ae1934
takano32 <mitsuhiro.takano@gree.co.jp>
2011-08-02 14:06:27 +0900

    add parse_url.py

---
+++ PyConJP2011/parse_url.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import pprint
+pp = pprint.PrettyPrinter(indent = 4)
+
+from urlparse import urlparse
+
+result = urlparse('http://example.com:4567/')
+print pp.pformat(result)
+
+result = urlparse('http://[fe80::1%en0]:7890/')
+print pp.pformat(result)
+
+
