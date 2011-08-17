2c9504f5b72fc0f9efd18371d732951f542a8810
takano32 <mitsuhiro.takano@gree.co.jp>
2011-08-17 13:06:21 +0900

    add PureImage library and it's test

---
+++ llplanets/v6echo/test_pureimage.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# vim: set noet ts=4 sts=4 sw=4:
+# author: takano32 <tak at no32.tk>
+#
+
+require 'pureimage'
+
+img = PureImage::Image.new(32, 32)
+io = PureImage::PNGIO.new
+io.save(img, "test_pureimage.png")
+
