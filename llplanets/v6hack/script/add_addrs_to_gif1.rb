8f1f43083e628f558f0c1b0db105d212a41be326
takano32 <takano32@gmail.com>
2011-08-20 13:58:26 +0900

    add add_addrs_to_gif1.rb to add addrs to FreeBSD

---
+++ llplanets/v6hack/script/add_addrs_to_gif1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby1.9
+# vim: set sts=2 sw=2 ts=2 ft=ruby :
+# sudo ifconfig gif1 inet6 add 2001:2e8:406:32::3
+IF='gif1'
+NETWORK='2001:2e8:406:11'
+
+if __FILE__ == $0 then
+  1.upto 2**128 do |i|
+    break if i == 2**64
+    addr = sprintf("%X\n", i).reverse.scan(/.{1,4}/).reverse.map do |hex|
+      hex.reverse
+    end.join(':')
+    puts %Q!sudo ifconfig #{IF} inet6 add #{NETWORK}::#{addr}/64!
+  end
+end
+
