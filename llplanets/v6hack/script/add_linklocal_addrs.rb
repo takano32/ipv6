d7556e852158ad4208792a7b92ea7ede86e5a7d5
takano32 <takano32@gmail.com>
2011-08-20 11:38:30 +0900

    add vim modeline

--- llplanets/v6hack/script/add_linklocal_addrs.rb
+++ llplanets/v6hack/script/add_linklocal_addrs.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby1.9
-
+# vim: set sts=2 sw=2 ts=2 ft=ruby :
 IF='en0'
 
 if __FILE__ == $0 then
-	1.upto 2**128 do |i|
-		break if i == 2**8
-		addr = sprintf("%X\n", i).reverse.scan(/.{1,4}/).reverse.map do |hex|
-			hex.reverse
-		end.join(':')
-		puts %Q!sudo ifconfig #{IF} inet6 add fe80::#{addr}/64!
-	end
+  1.upto 2**128 do |i|
+    break if i == 2**8
+    addr = sprintf("%X\n", i).reverse.scan(/.{1,4}/).reverse.map do |hex|
+      hex.reverse
+    end.join(':')
+    puts %Q!sudo ifconfig #{IF} inet6 add fe80::#{addr}/64!
+  end
 end
 
