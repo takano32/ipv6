f9691998af2078979e871f599578b7c70fe724e5
takano32 <takano32@gmail.com>
2011-08-20 13:21:28 +0900

    impl int2addr.rb

--- llplanets/v6hack/script/int2addr.rb
+++ llplanets/v6hack/script/int2addr.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby1.9
 # vim: set sts=2 sw=2 ts=2 ft=ruby :
 
-100000.to_s(16).reverse.scan(/.{1,4}/).join(':').reverse
+exit if ARGV.empty?
+
+id = ARGV.join.to_i
+puts id.to_s(16).reverse.scan(/.{1,4}/).join(':').reverse
+
 
