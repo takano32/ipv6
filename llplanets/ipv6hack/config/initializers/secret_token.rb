ce5a7a8fe2283b4159e291e0c1d3df671961ae1f
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-08-20 10:50:29 +0900

    Create ralis project for IPv6 Hackathon

---
+++ llplanets/ipv6hack/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipv6hack::Application.config.secret_token = '2dc0aa5574802fb6815b951ffeca9ff50eb4dd9499691d6c1dc12c4c8fa7f9f883224a9b75db2a2ee19cef2c0a12a7134f23c785b880d1d8953e495b754248ba'
