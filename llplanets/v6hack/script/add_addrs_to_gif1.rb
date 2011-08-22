5ca074d8751f06e4e3c595683c727819aed8e951
takano32 <mitsuhiro.takano@gree.co.jp>
2011-08-22 11:14:08 +0900

    add fix route command as comment

--- llplanets/v6hack/script/add_addrs_to_gif1.rb
+++ llplanets/v6hack/script/add_addrs_to_gif1.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby1.9
 # vim: set sts=2 sw=2 ts=2 ft=ruby :
 # sudo ifconfig gif1 inet6 add 2001:2e8:406:32::3
+#
+# sudo route delete -inet6 default
+# sudo route  add -inet6 default 2001:2e8:406:11::1
+#
+
 IF='gif1'
 NETWORK='2001:2e8:406:11'
 
