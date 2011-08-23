055eb86fb3320f4a1d6d04d75d44d6c02450650e
takano32 <takano32@gmail.com>
2011-08-23 18:56:11 +0900

    add file to assign color like addresses

---
+++ llplanets/v6hack/script/add_color_addrs_to_gif1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby1.9
+# vim: set sts=2 sw=2 ts=2 ft=ruby :
+# sudo ifconfig gif1 inet6 add 2001:2e8:406:32::3
+#
+# sudo route delete -inet6 default
+# sudo route  add -inet6 default 2001:2e8:406:11::1
+#
+
+IF='gif1'
+NETWORK='2001:2e8:406:11'
+
+if __FILE__ == $0 then
+  0.upto 2**128 do |i|
+    break if i == 2**24-1
+    addr = sprintf("%X\n", i).reverse.scan(/.{1,2}/).reverse.map do |hex|
+      hex.reverse
+    end.join(':')
+    puts %Q!sudo ifconfig #{IF} inet6 add #{NETWORK}::#{addr}:0/64!
+  end
+end
+
