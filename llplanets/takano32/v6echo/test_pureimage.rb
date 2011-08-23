61e7555e44236d2998d7588901299afcc59996db
takano32 <takano32@gmail.com>
2011-08-23 15:38:10 +0900

    fix test_pureimage, but cant use at ruby 1.9

--- llplanets/takano32/v6echo/test_pureimage.rb
+++ llplanets/takano32/v6echo/test_pureimage.rb
@@ -3,8 +3,9 @@
 # author: takano32 <tak at no32.tk>
 #
 
-require 'pureimage'
+require './pureimage'
 
+sio = StringIO.new
 img = PureImage::Image.new(32, 32)
 io = PureImage::PNGIO.new
 io.save(img, "test_pureimage.png")
